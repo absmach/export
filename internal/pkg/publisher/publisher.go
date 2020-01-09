7405389699149c979d03913444f18f022b556e3e
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-01-09 15:31:58 +0100

    using cloudflare
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

---
+++ internal/pkg/publisher/publisher.go
@@ -0,0 +1,68 @@
+package publisher
+
+import (
+	"encoding/json"
+	"github.com/cloudflare/buffer"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/mainflux/export/internal/app/export"
+	"time"
+)
+
+const (
+	filename = "store.dat"
+	filesize = 262144000
+)
+
+type record struct {
+	time  time.Time
+	topic string
+	bytes []byte
+}
+
+type storage struct {
+	buff *buffer.Buffer
+	mqtt mqtt.Client
+	exp  export.Exporter
+}
+type File interface {
+	Publish(b []byte, topic string) (err error)
+}
+
+func NewPublisher(name string, m mqtt.Client) (File, error) {
+	s := storage{}
+	b, err := buffer.New(name, filesize)
+	if err != nil {
+		return &s, err
+	}
+	s.buff = b
+	s.mqtt = m
+	return &s, nil
+}
+func (s *storage) write(b []byte, topic string) (err error) {
+	time := time.Now()
+	r := record{time, topic, b}
+	w, err := json.Marshal(r)
+	if err != nil {
+		return err
+	}
+	err = s.buff.Insert(w)
+	return err
+}
+
+func (s *storage) Publish(b []byte, topic string) (err error) {
+
+	return nil
+}
+
+func (s *storage) read() (b []byte, err error) {
+	b, err = s.buff.Pop()
+	if err != nil {
+		return nil, err
+	}
+	r := record{}
+	err = json.Unmarshal(b, r)
+	if err != nil {
+		return nil, err
+	}
+	return r.bytes, nil
+}
