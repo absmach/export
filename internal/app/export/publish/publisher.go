6b2e8ff4a771821d1dec9fe9aac60ead02f9f63f
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2020-03-17 11:20:12 +0100

    refactor, change folder structure
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- internal/app/export/publish/publisher.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Mainflux
-// SPDX-License-Identifier: Apache-2.0
-package publish
-
-import "github.com/mainflux/mainflux/errors"
-
-type Publisher interface {
-	Publish(string, string, []byte) errors.Error
-}
